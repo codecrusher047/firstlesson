6f5b1a03d9c2dc321faa09478cd57b9167882188
Eve Adongo <adongoe678@gmail.com>
2024-11-20 17:32:27 +0300

    test for fill and fill too much

--- CupTest.java
+++ CupTest.java
@@ -13,4 +13,26 @@ public class CupTest {
 
         assertEquals(0, cup.content);
     }
+    @Test
+    public void testfillCup() {
+        Cup cup = new Cup("Eve");
+        cup.size = 200;
+
+        cup.fill(50);
+        cup.fill (100);
+    
+
+        assertEquals(150, cup.content);
+    }
+    @Test
+    public void testfillCupToomuch() {
+        Cup cup = new Cup("Eve");
+        cup.size = 200;
+
+        cup.fill(50);
+        cup.fill (300);
+    
+
+        assertEquals(200, cup.content);
+    }
 }
